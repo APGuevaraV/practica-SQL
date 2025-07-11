e70e4f418e947e7aaa9bb36ac6c61bf1ca01da23
Ana Paula <ana.guevara.valladares@gmail.com>
2025-07-11 17:06:13 -0500

    ordenacion ascendente y descendente

---
+++ Statements/order_by.sql
@@ -0,0 +1,3 @@
+SELECT * FROM users ORDER BY age DESC;
+SELECT * FROM users ORDER BY age ASC;
+SELECT * FROM users  WHERE email='ana@gmail.com' ORDER BY age ASC;
--- Statements/where.sql
+++ Statements/where.sql
@@ -1,1 +1,3 @@
 SELECT * FROM users WHERE age=15;
+SELECT DISTINCT age FROM users WHERE age=15;
+SELECT DISTINCT name FROM users WHERE age=15;
