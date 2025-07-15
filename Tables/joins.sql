c4cc3a58cfa857baf22a1cd3b73343604436de63
Ana Paula <ana.guevara.valladares@gmail.com>
2025-07-15 17:15:47 -0500

    inner join en relacion de n*n

--- Tables/joins.sql
+++ Tables/joins.sql
@@ -1,3 +1,5 @@
+--inner join
+
 SELECT * FROM users INNER JOIN dni;
 SELECT * FROM users INNER JOIN dni 
 on users.user_id = dni.user_id;
@@ -8,3 +10,10 @@ on users.user_id = dni.user_id;
 SELECT name,dni_number FROM 
 users inner join dni 
 on users.user_id = dni.user_id  ORDER BY age;
+
+--1:n
+SELECT users.name,lenguajes.name 
+FROM users_lenguajes
+join users
+on users_lenguajes.user_id = users.user_id
+join lenguajes on users_lenguajes.lenguaje_id= lenguajes.lenguaje_id;
